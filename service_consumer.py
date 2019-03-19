eb390ced4cf5f2a571e48c82f747e1f1a111d363
ajoino <jacob.nilsson@ltu.se>
2019-03-19 09:12:46 +0100

    Added functionality to the service consumer class

--- service_consumer.py
+++ service_consumer.py
@@ -1,7 +1,57 @@
 import requests
+from arrowhead_system import ArrowheadSystem
 
 class ServiceConsumer():
-    pass
+    def __init__(self,
+            consumer_name = 'Default',
+            address = 'localhost',
+            port = '8080',
+            authentication_info = '',
+            consumer_system = None,
+            requested_service = {},
+            orchestration_flags = {},):
+        if consumer_system:
+            self.system = consumer_system
+        else:
+            self.system = ArrowheadSystem(consumer_name, 
+                    address, 
+                    port, 
+                    authentication_info)
+        self.req_service = requested_service
+        self.orch_flags = orchestration_flags
+
+    @property
+    def system_name(self):
+        return self.system.system_name
+
+    @property
+    def address(self):
+        return self.system.address
+
+    @property
+    def port(self):
+        return self.system.port
+
+    def service_request_form(self, 
+            requested_service = {},
+            orchestration_flags = {}):
+        service_request_form = {
+                'requesterSystem': {
+                    'systemName': self.name,
+                    'address': self.host,
+                    'port': self.port,
+                    'authentificationInfo': 'null'
+                    },
+                'requestedService': requested_service,
+                'orchestrationFlags': orchestration_flags,
+                'preferredProviders': {},
+                'requestedQoS': {},
+                'commands': {}
+                }
 
 if __name__ == '__main__':
-    pass
+    consumer = ServiceConsumer()
+    a = consumer.system_name
+    print(a)
+
+    
