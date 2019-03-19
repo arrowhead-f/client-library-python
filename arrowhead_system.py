ee5b2696f460867e28c169048dad9e32e44e21aa
ajoino <jacob.nilsson@ltu.se>
2019-03-19 08:50:20 +0100

    Changed the authentication info to not be included in comparison operator

--- arrowhead_system.py
+++ arrowhead_system.py
@@ -1,11 +1,11 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field, asdict
 
 @dataclass
 class ArrowheadSystem():
     system_name: str = 'Default'
     address: str = 'localhost'
     port: str = '8080'
-    authentication_info: str = ''
+    authentication_info: str = field(compare=False, default='')
     
 if __name__ == '__main__':
     test_system = ArrowheadSystem()
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     false_name_system = ArrowheadSystem(system_name='default')
     false_addr_system = ArrowheadSystem(address='0.0.0.0')
     false_port_system = ArrowheadSystem(port='8081')
+    other_auth_system = ArrowheadSystem(authentication_info = 'test')
     print(test_system)
 
     print(test_system == test_system)
@@ -20,3 +21,6 @@ if __name__ == '__main__':
     print(test_system == false_name_system)
     print(test_system == false_addr_system)
     print(test_system == false_port_system)
+    print(test_system == other_auth_system)
+
+    print(asdict(test_system))
