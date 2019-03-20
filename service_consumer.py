f57eec0ccb6be478126c63cd6106ce2c126f05c9
ajoino <jacob.nilsson@ltu.se>
2019-03-20 19:07:26 +0100

    Working on the consumer now, need to look more closely at the serviceRequestForm and make sure I can get it. Also, I need to make sure it looks up the orchestrator and authentication system's addresses so it only needs the service registry's to start.

--- service_consumer.py
+++ service_consumer.py
@@ -3,26 +3,29 @@ from arrowhead_system import ArrowheadSystem
 
 class ServiceConsumer():
     def __init__(self,
-            consumer_name = 'Default',
+            name = 'Default',
             address = 'localhost',
             port = '8080',
             authentication_info = '',
             consumer_system = None,
             requested_service = {},
-            orchestration_flags = {},):
+            orchestration_flags = {},
+            service_registry = None):
         if consumer_system:
             self.system = consumer_system
         else:
-            self.system = ArrowheadSystem(consumer_name, 
+            self.system = ArrowheadSystem(name, 
                     address, 
                     port, 
                     authentication_info)
         self.req_service = requested_service
         self.orch_flags = orchestration_flags
 
+        assert service_registry
+
     @property
     def system_name(self):
-        return self.system.system_name
+        return self.system.systemName
 
     @property
     def address(self):
@@ -36,18 +39,17 @@ class ServiceConsumer():
             requested_service = {},
             orchestration_flags = {}):
         service_request_form = {
-                'requesterSystem': {
-                    'systemName': self.name,
-                    'address': self.host,
-                    'port': self.port,
-                    'authentificationInfo': 'null'
-                    },
+                'requesterSystem': self.consumer_system.no_auth,
                 'requestedService': requested_service,
                 'orchestrationFlags': orchestration_flags,
                 'preferredProviders': {},
                 'requestedQoS': {},
                 'commands': {}
                 }
+        return service_request_form
+
+    def consume(self):
+        pass
 
 if __name__ == '__main__':
     consumer = ServiceConsumer()
