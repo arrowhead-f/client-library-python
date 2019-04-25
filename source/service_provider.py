2845317231a626efc9b9b9357b9ba7433586cfe1
ajoino <jacob.nilsson@ltu.se>
2019-04-25 10:31:53 +0200

    Fixed bug where all authorization forms were empty

--- source/service_provider.py
+++ source/service_provider.py
@@ -51,11 +51,9 @@ class ServiceProvider:
             assert self.consumer_system
         except AssertionError:
             print('Consumer system must be defined for creation of athentication entry')
-        finally:
-            return None
 
         auth_entry = {
-                'consumer': self.consumer_system.no_auth(),
+                'consumer': self.consumer_system.no_auth,
                 'providerList': [self.provider_system.no_auth],
                 'serviceList': [{
                     'serviceDefinition': self.name,
@@ -84,8 +82,10 @@ class ServiceProvider:
 
     def authenticate(self):
         """authenticate service"""
-        pprint(f'authenticate: {self._auth_entry()}')
+        print('Authentication entry')
+        pprint(self._auth_entry())
         r = requests.post('http://127.0.0.1:8444/authorization/mgmt/intracloud', json=self._auth_entry())
+        pprint(r.json())
         assert r.ok
 
     def publish(self):
@@ -116,12 +116,13 @@ class ServiceProvider:
 if __name__=='__main__':
     """ A bunch of test code """
     test_system = ArrowheadSystem(systemName='Test', address='127.0.0.1', port='9345')
-    test_provider = ServiceProvider(service_uri='/TestProvider', provider_system=test_system)
+    test_consumer = ArrowheadSystem('Test_Consumer', '127.0.0.1', '6006')
+    test_provider = ServiceProvider(service_uri='/TestProvider', provider_system=test_system, consumer_system=test_consumer)
     pprint(test_provider.sr_entry)
     def hello(name):
         return f'hello {name}\n'
 
-    test_provider.add_route(function_uri='/test', func=hello, name='Jacob', auth=True)
-    print(test_provider.service_routes)
+    test_provider.add_route(function_uri='/test', func=hello, name='Jacob')
+    #print(test_provider.service_routes)
 
-    test_provider.run(auth=False)
+    test_provider.run(auth=True)
