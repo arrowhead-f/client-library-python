03aaa6ec24eac7594dd4ad33828e105cceed7718
ajoino <jacob.nilsson@ltu.se>
2020-11-10 14:18:15 +0100

    fixed error in setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9'
+            'Programming Language :: Python :: 3.9',
             'Operating System :: POSIX :: Linux',
             'License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)',
         ],
