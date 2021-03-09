539cc7710931d9b99e4977d90a881af527333bf9
ajoino <jacob.nilsson@ltu.se>
2021-03-09 11:47:59 +0100

    protocol argument to the ProtocolMixin can now be of type set. This is so the FastAPI provider (and future providers) can properly state it supports both HTTP and WebSockets.

--- arrowhead_client/abc.py
+++ arrowhead_client/abc.py
@@ -1,10 +1,14 @@
 from abc import ABC
 
-
 class ProtocolMixin(ABC):
     def __init_subclass__(cls, protocol='', **kwargs):
         if protocol == '':
             raise ValueError('No protocol specified.')
-        elif not isinstance(protocol, str):
-            raise TypeError('Protocol must be of type str.')
-        cls._protocol = protocol.upper()
+        elif isinstance(protocol, set):
+            if not all(isinstance(prot, str) for prot in protocol):
+                raise ValueError('All protocols specified must be of type str')
+            cls._protocol = set(prot.upper() for prot in protocol)
+        elif isinstance(protocol, str):
+            cls._protocol = {protocol.upper()}
+        else:
+            raise ValueError('protocol must be of type str or set')
