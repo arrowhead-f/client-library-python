492f8487d7c8c13b5d1ab882505b4ed34a0bf213
ajoino <jacob.nilsson@ltu.se>
2022-05-27 23:14:44 +0200

    Reformatted file with Black

--- src/arrowhead_client/client/core_service_responses.py
+++ src/arrowhead_client/client/core_service_responses.py
@@ -26,9 +26,13 @@ def core_service_error_handler(func) -> Callable:
     @wraps(func)
     def error_handling_wrapper(core_service_response: Response, *args, **kwargs):
         if core_service_response.status_code == 401:
-            raise errors.NotAuthorizedError(core_service_response.read_json()[constants.Misc.ERROR_MESSAGE])
+            raise errors.NotAuthorizedError(
+                core_service_response.read_json()[constants.Misc.ERROR_MESSAGE]
+            )
         elif core_service_response.status_code == 500:
-            raise errors.CoreServiceNotAvailableError(core_service_response.read_json()[constants.Misc.ERROR_MESSAGE])
+            raise errors.CoreServiceNotAvailableError(
+                core_service_response.read_json()[constants.Misc.ERROR_MESSAGE]
+            )
 
         return func(core_service_response, *args, **kwargs)
 
@@ -36,25 +40,31 @@ def core_service_error_handler(func) -> Callable:
 
 
 @core_service_error_handler
-def process_service_query(query_response: Response) -> List[Tuple[Service, ArrowheadSystem]]:
-    """ Handles provided_service query responses and returns a lists of services and systems """
+def process_service_query(
+    query_response: Response,
+) -> List[Tuple[Service, ArrowheadSystem]]:
+    """Handles provided_service query responses and returns a lists of services and systems"""
     # TODO: Status 400 is general for all core systems and should be put in the handler.
     if query_response.status_code == 400:
-        raise errors.CoreServiceInputError(query_response.read_json()[constants.Misc.ERROR_MESSAGE])
+        raise errors.CoreServiceInputError(
+            query_response.read_json()[constants.Misc.ERROR_MESSAGE]
+        )
 
     query_response_ = forms.ServiceQueryResponse(**query_response.read_json())
 
     service_and_system = [
         (
             Service(
-                    service_definition=query_result.service_definition.service_definition,
-                    service_uri=query_result.service_uri,
-                    interface=ServiceInterface.from_str(query_result.interfaces[0].interface_name),
-                    access_policy='',
-                    metadata=query_result.metadata,
-                    version=query_result.version,
+                service_definition=query_result.service_definition.service_definition,
+                service_uri=query_result.service_uri,
+                interface=ServiceInterface.from_str(
+                    query_result.interfaces[0].interface_name
+                ),
+                access_policy="",
+                metadata=query_result.metadata,
+                version=query_result.version,
             ),
-            ArrowheadSystem(**query_result.provider.dict())
+            ArrowheadSystem(**query_result.provider.dict()),
         )
         for query_result in query_response_.service_query_data
     ]
@@ -64,10 +74,10 @@ def process_service_query(query_response: Response) -> List[Tuple[Service, Arrow
 
 @core_service_error_handler
 def process_service_register(service_register_response: Response):
-    """ Handles service registration responses """
+    """Handles service registration responses"""
     if service_register_response.status_code == 400:
         raise errors.CoreServiceInputError(
-                service_register_response.read_json()[constants.Misc.ERROR_MESSAGE],
+            service_register_response.read_json()[constants.Misc.ERROR_MESSAGE],
         )
     # TODO: Should return a string representing the successfully registered service for logging?
     return forms.ServiceRegistryEntry(**service_register_response.read_json())
@@ -75,16 +85,18 @@ def process_service_register(service_register_response: Response):
 
 @core_service_error_handler
 def process_service_unregister(service_unregister_response: Response) -> None:
-    """ Handles service unregistration responses """
+    """Handles service unregistration responses"""
     if service_unregister_response.status_code == 400:
         raise errors.CoreServiceInputError(
-                service_unregister_response.read_json()[constants.Misc.ERROR_MESSAGE]
+            service_unregister_response.read_json()[constants.Misc.ERROR_MESSAGE]
         )
     # TODO: Should return a string representing the successfully unregistered service for logging?
 
 
 @core_service_error_handler
-def process_orchestration(orchestration_response: Response, method='') -> List[OrchestrationRule]:
+def process_orchestration(
+    orchestration_response: Response, method=""
+) -> List[OrchestrationRule]:
     """
     Turns orchestration response into list of services.
 
@@ -95,10 +107,12 @@ def process_orchestration(orchestration_response: Response, method='') -> List[O
         List of OrchestrationRules found in the orchestration response.
     """
     if orchestration_response.status_code == 400:
-        raise errors.OrchestrationError(orchestration_response.read_json()[constants.Misc.ERROR_MESSAGE])
+        raise errors.OrchestrationError(
+            orchestration_response.read_json()[constants.Misc.ERROR_MESSAGE]
+        )
 
     orchestration_results = forms.OrchestrationResponseList(
-            **orchestration_response.read_json()
+        **orchestration_response.read_json()
     ).response
 
     extracted_rules = [
@@ -117,8 +131,8 @@ def process_publickey(publickey_response: Response) -> str:
 
 
 def _extract_orchestration_rules(
-        orchestration_result: forms.OrchestrationResponse,
-        method,
+    orchestration_result: forms.OrchestrationResponse,
+    method,
 ) -> OrchestrationRule:
     """
     Helper function to extract orchestration rules from an orchestration result.
@@ -137,30 +151,30 @@ def _extract_orchestration_rules(
 
     interface = service_dto.interfaces[0].interface_name
     auth_tokens = service_dto.authorization_tokens
-    auth_token = auth_tokens.get(interface, '') if auth_tokens else ''
+    auth_token = auth_tokens.get(interface, "") if auth_tokens else ""
 
     return OrchestrationRule(service, system, method, auth_token)
 
 
 def _extract_service(query_data: forms.OrchestrationResponse) -> Service:
-    """ Extracts provided_service data from test_core provided_service response """
+    """Extracts provided_service data from test_core provided_service response"""
     # TODO: this code guarded against different versions of OrchestrationResponse, not sure why
-    '''
+    """
     if 'serviceDefinition' in query_data.dict():
         service_definition_base = 'serviceDefinition'
     elif 'service' in query_data.dict():
         service_definition_base = 'service'
     else:
         raise ValueError
-    '''
+    """
 
     service = Service(
-            query_data.service.service_definition,
-            query_data.service_uri,
-            ServiceInterface.from_str(query_data.interfaces[0].interface_name),
-            query_data.secure,
-            query_data.metadata,
-            query_data.version,
+        query_data.service.service_definition,
+        query_data.service_uri,
+        ServiceInterface.from_str(query_data.interfaces[0].interface_name),
+        query_data.secure,
+        query_data.metadata,
+        query_data.version,
     )
 
     return service
